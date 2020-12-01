c5d4f9a315b7549259b6246445fbb46e4754fdc7
jyliu17 <jyliu17@gmail.com>
2020-12-01 17:07:10 -0500

    artist mvc

--- app/controllers/artist_specialities_controller.rb
+++ app/controllers/artist_specialities_controller.rb
@@ -1,2 +1,4 @@
 class ArtistSpecialitiesController < ApplicationController
+
+    
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,6 @@
 class Artist < ApplicationRecord
+    has_many :project_artists
+    has_many :projects, through: :project_artists
+    has_many :artist_specialties
+    has_many :specialties, through: :artist_specialties 
 end
