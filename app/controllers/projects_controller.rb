521fc9637010220fdd89a7942e51653667774c1a
bstizzle <whereyouben@gmail.com>
2020-12-01 16:51:14 -0500

    wrote generic setup for projects and specialities

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,42 @@
 class ProjectsController < ApplicationController
+    def index
+        @projects = Project.all
+    end
+
+    def show
+        @project = Project.find(params[:id])
+    end 
+
+    def new
+        @project = Project.new
+    end 
+
+    def create
+        @project = Project.create(project_params)
+
+        redirect_to project_path(@project)
+    end 
+
+    def edit
+        @project = Project.find(params[:id])
+    end
+
+    def update
+        @project = Project.find(params[:id])
+
+        redirect_to project_path(@project)
+    end 
+
+    def destroy
+        @project = Project.find(params[:id])
+        @project.delete
+        
+        redirect_to projects_path
+    end
+
+    private
+
+    def project_params
+        params.require(:project).permit(:name, :description, :deadline, :location, :creator_id)
+    end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ApplicationRecord
+    belongs_to :creator
+    has_many :project_artists
+    has_many :artists, through: :project_artists
+    
 end
