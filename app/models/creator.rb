15dcd4f96e50be18cf4c1a70cd79b4d080f37700
Ekenayy <ekenayy@gmail.com>
2020-12-01 16:41:39 -0500

    Created relationships for creator and projectartists

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -1,2 +1,5 @@
 class Creator < ApplicationRecord
+
+    has_many :projects
+    has_many :artists, through: :projects
 end
--- app/models/project_artist.rb
+++ app/models/project_artist.rb
@@ -1,2 +1,5 @@
 class ProjectArtist < ApplicationRecord
+    belongs_to :project
+    belongs_to :artist
+
 end
